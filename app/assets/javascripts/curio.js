1d707a5e71fb4014224d4e217a09e23b40d02413
lala <whatwasthatonethen@gmail.com>
2014-06-04 10:32:19 +0200

    Made js run after turbolinks load
    Because of turbolinks, clicking a link to another page wasn't running
    the javascript.

--- app/assets/javascripts/curio.js
+++ app/assets/javascripts/curio.js
@@ -1,18 +1,23 @@
-$(window).load(function(){
+var CURIO = function() {
 	
-	//Masonry
-	$('#posts').imagesLoaded( function(){
-	    $('#posts').isotope({
-	      itemSelector : '.item',
-	      masonry: {
-	        cornerStampSelector: '.corner-stamp',
-	        columnWidth : 20
-	      }
-	    });
-    });
-    
-    //Share buttons
-    Socialite.load($('#share-buttons'));
+	return {
 
-});
+	  init : function() {
+		//Masonry
+		$('#posts').imagesLoaded( function(){
+		$('#posts').isotope({
+		  itemSelector : '.item',
+		  masonry: {
+		    cornerStampSelector: '.corner-stamp',
+		        columnWidth : 20
+		      }
+		    });
+		});
+		
+		//Share buttons
+		Socialite.load($('#share-buttons'));
+	   }
+	}
+}();
 
+$(window).bind('page:change', CURIO.init);
