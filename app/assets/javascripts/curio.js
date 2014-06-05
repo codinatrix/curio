fe354633331998883bff773b47f13c195306ee29
lala <whatwasthatonethen@gmail.com>
2014-06-05 18:50:29 +0200

    Made the map purdy

--- app/assets/javascripts/curio.js
+++ app/assets/javascripts/curio.js
@@ -19,6 +19,138 @@ var CURIO = function() {
 			//Share buttons
 			Socialite.load($('#share-buttons'));
 			
+			
+			//Google Maps
+			var map_style =	[
+			    {
+			        "featureType": "water",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#004358"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "landscape",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#1f8a70"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#1f8a70"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.highway",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#fd7400"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.arterial",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#1f8a70"
+			            },
+			            {
+			                "lightness": -20
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road.local",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#1f8a70"
+			            },
+			            {
+			                "lightness": -17
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.text.stroke",
+			        "stylers": [
+			            {
+			                "color": "#ffffff"
+			            },
+			            {
+			                "visibility": "on"
+			            },
+			            {
+			                "weight": 0.9
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.text.fill",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            },
+			            {
+			                "color": "#ffffff"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi",
+			        "elementType": "labels",
+			        "stylers": [
+			            {
+			                "visibility": "simplified"
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.icon",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "transit",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#1f8a70"
+			            },
+			            {
+			                "lightness": -10
+			            }
+			        ]
+			    },
+			    {},
+			    {
+			        "featureType": "administrative",
+			        "elementType": "geometry",
+			        "stylers": [
+			            {
+			                "color": "#1f8a70"
+			            },
+			            {
+			                "weight": 0.7
+			            }
+			        ]
+			    }
+			]
+			
 			google.maps.event.addDomListener(window, 'load', function(){
 			var map_canvas = document.getElementById('map_canvas');
 			var mark = $('marker:first');
@@ -26,7 +158,8 @@ var CURIO = function() {
 		    var map_options = {
 		      center: lat_lng,
 		      zoom: 15,
-		      mapTypeId: google.maps.MapTypeId.ROADMAP
+		      mapTypeId: google.maps.MapTypeId.ROADMAP,
+		      styles: map_style
 		    }
 		    var map = new google.maps.Map(map_canvas, map_options);
 		    var marker = new google.maps.Marker({
