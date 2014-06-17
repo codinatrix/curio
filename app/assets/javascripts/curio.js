8cea33bf762fb5c7a58d01530d43613fba858e70
lala <whatwasthatonethen@gmail.com>
2014-06-17 21:39:13 +0200

    Refactored js to objects

--- app/assets/javascripts/curio.js
+++ app/assets/javascripts/curio.js
@@ -1,25 +1,25 @@
 var CURIO = function() {
+		
+		var masonyClass = function() {
+			
+			this.init = function() {
+				posts = $('#posts');
+				posts.imagesLoaded( function(){
+				posts.isotope({
+				  itemSelector : '.item',
+				  masonry: {
+				    cornerStampSelector: '.corner-stamp',
+				        columnWidth : 20
+				      }
+				   });
+				   posts.isotope('layout');
+				});
+			}
 	
-	return {
+		}// End of masonryClass
+		
+		var mapClass = function() {
 
-		init : function() {
-			//Masonry
-			posts = $('#posts');
-			posts.imagesLoaded( function(){
-			posts.isotope({
-			  itemSelector : '.item',
-			  masonry: {
-			    cornerStampSelector: '.corner-stamp',
-			        columnWidth : 20
-			      }
-			   });
-			   posts.isotope('layout');
-			});
-			
-			//Share buttons
-			Socialite.load($('#share-buttons'));
-			
-			
 			//Google Maps
 			var map_style =	[
 			    {
@@ -151,34 +151,56 @@ var CURIO = function() {
 			    }
 			]
 			
-			google.maps.event.addDomListener(window, 'page:change', function(){
-				var map_canvas = document.getElementById('map_canvas');
-				var mark = $('marker:first');
-				var lat_lng = new google.maps.LatLng(mark.attr('latitude'),mark.attr('longitude'));
-			    var map_options = {
-			      center: lat_lng,
-			      zoom: 15,
-			      mapTypeId: google.maps.MapTypeId.ROADMAP,
-			      styles: map_style
-			    }
-			    var map = new google.maps.Map(map_canvas, map_options);
-			    var marker = new google.maps.Marker({
-				  position: lat_lng,
-				  map: map,
-				  title: 'Hello World!'
-				});
-			});
+			var googleInit = function() {
+				if ($("#map_canvas").length > 0) {
+					var map_canvas = document.getElementById('map_canvas');
+					var mark = $('marker:first');
+					var lat_lng = new google.maps.LatLng(mark.attr('latitude'),mark.attr('longitude'));
+				    var map_options = {
+				      center: lat_lng,
+				      zoom: 15,
+				      mapTypeId: google.maps.MapTypeId.ROADMAP,
+				      styles: map_style
+				    }
+				    var map = new google.maps.Map(map_canvas, map_options);
+				    var marker = new google.maps.Marker({
+					  position: lat_lng,
+					  map: map
+					});
+				}
+			}
+
 			
-			/*
-			 * Alert
-			 */
-			$(".alert button.close").click(function (e) {
-			    $(this).parent().slideUp('slow');
-			});
+			this.init = function() {
+				google.maps.event.addDomListener(window, 'page:change', googleInit);
+				google.maps.event.addDomListener(window, 'load', googleInit);
+			}
+	
+		}// End of mapClass
+		
+	return {
+		init : function() {
+			
+			if ($("#posts").length > 0) {
+				//Alert
+				$(".alert button.close").click(function (e) {
+			    	$(this).parent().slideUp('slow');
+				});
+				var masonyObj = new masonyClass();
+				masonyObj.init();
+			}
+			else if ($("#map_canvas").length > 0) {
+				var mapObj = new mapClass();
+				mapObj.init();
+				
+				//Share buttons
+				Socialite.load($('#share-buttons'));
+			}
 			
 		}//End of Init
 	}
 }();
 
 $(window).bind('page:change', CURIO.init);
+
 
